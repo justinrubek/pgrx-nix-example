d110fb65b6b60f960993d6d765fe37384c412966
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-04-28 14:52:17 -0500

    feat: expose postgres with extension
    This uses services-flake to run postgres locally, but also exposes the
    `postgresql` flake package when can be used to run postgres. To run it
    locally, use `nix run .#services`. This command will expect environment
    variables set: `PRJ_DATA_HOME` and `PG1_PORT`. For convenience, direnv
    will look in the `.direnv/env` directory for files that define
    environment variables (the file name is the variable name and file
    contents are its value).

---
+++ flake-parts/services.nix
@@ -0,0 +1,49 @@
+{inputs, ...} @ part-inputs: {
+  imports = [];
+
+  perSystem = {
+    pkgs,
+    inputs',
+    self',
+    ...
+  }: let
+    # define some values that are used in multiple places.
+    # this is used to ensure that all services data is kept in the same directory, and that
+    # each service has its own directory within the main one.
+    # if there were flake input variables we could determine the dataDir from those, but
+    # since there aren't we will allow for an environment variable and a sensible default.
+    ports = {
+      pg1 = "\${PG1_PORT}";
+    };
+
+    global-imports = [
+      ({name, ...}: {
+        dataDirEnv = "\${PRJ_DATA_HOME}/${name}";
+        socketDirEnv = "\${PRJ_DATA_HOME}/${name}/sockets";
+      })
+    ];
+  in rec {
+    process-compose = {
+      services = {
+        imports = [
+          inputs.services-flake.processComposeModules.default
+        ];
+
+        services.postgres."pg1" = {
+          imports = global-imports;
+
+          enable = true;
+          package = self'.packages.postgresql;
+
+          listen_addresses = "127.0.0.1";
+          port = ports."pg1";
+          initialDatabases = [
+            {
+              name = "array-example";
+            }
+          ];
+        };
+      };
+    };
+  };
+}
--- flake.nix
+++ flake.nix
@@ -16,21 +16,32 @@
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    process-compose.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:justinrubek/services-flake";
   };
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux"];
+      imports = [
+        inputs.process-compose.flakeModule
+        ./flake-parts/services.nix
+        ./flake-parts/postgres.nix
+      ];
       perSystem = {
         pkgs,
         system,
         inputs',
+        self',
         ...
       }: {
+        devShells.default = pkgs.mkShell {
+          packages = [self'.packages.postgresql];
+        };
         packages = {
           pgrx-arrays = inputs.pgrx.lib.buildPgrxExtension {
             inherit system;
-            postgresql = pkgs.postgresql_15;
+            postgresql = self'.packages.postgresql-target;
             rustToolchain = inputs'.fenix.packages.stable.toolchain;
             src = inputs.nix-filter.lib {
               root = ./.;
